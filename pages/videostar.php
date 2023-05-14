ef0e6905bbf7be907654ce68d04f4c456f41428a
HAMAEXPERT <77998844+hamacracker@users.noreply.github.com>
2023-05-15 02:17:56 +0300

    Add files via upload

--- pages/videostar.php
+++ pages/videostar.php
@@ -32,7 +32,7 @@
                 <div class="item-title-row">
                   <div class="item-title">
                     VideoStar++
-                  </div><a href="itms-services://?action=download-manifest&url=https://hamastore.netlify.app//plist/videostarold.plist">
+                  </div><a href="itms-services://?action=download-manifest&url=https://hamastore.netlify.app//plist/videostarold.plist"
                   class=
                   "button button-round button-fill button-raised color-green external">Get
                   <i class=
